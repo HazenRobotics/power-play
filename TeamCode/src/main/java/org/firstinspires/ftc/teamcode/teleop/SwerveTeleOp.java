52ace49f656a85e150bf6442d682e379840a61e5
Sam DePoule <samdepoule@gmail.com>
2022-09-28 16:58:58 -0700

    Upgrading to 8.0.0

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/SwerveTeleOp.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.teamcode.utils.GamepadEvents;
+
+@TeleOp(name = "SwerveTeleOp", group = "Test")
+//@Disabled
+public class SwerveTeleOp extends OpMode {
+
+	DcMotor leftMotor, rightMotor;
+
+	@Override
+	public void init( ) {
+
+		telemetry.addData( "Mode", "Initiating robot..." );
+		telemetry.update( );
+
+		leftMotor = hardwareMap.dcMotor.get( "left" );
+		rightMotor = hardwareMap.dcMotor.get( "right" );
+
+		telemetry.addData( "Mode", "waiting for start??" );
+		telemetry.update( );
+	}
+
+	@Override
+	public void loop( ) {
+
+		leftMotor.setPower( gamepad1.left_stick_y );
+		rightMotor.setPower( gamepad1.right_stick_y );
+
+		telemetry.addData( "l: ", gamepad1.left_stick_y );
+		telemetry.addData( "r: ", gamepad1.right_stick_y );
+
+		telemetry.update( );
+	}
+
+}
