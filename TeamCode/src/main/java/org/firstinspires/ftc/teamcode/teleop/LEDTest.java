bb0a0532d2c8eca23df617a8b3f6742807cf6aed
Connerbruner <72578768+Connerbruner@users.noreply.github.com>
2022-10-10 14:33:15 -0700

    LED & Swerve Testing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/LEDTest.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.utils.RGBLights;
+
+@Autonomous
+public class LEDTest extends LinearOpMode {
+
+	@Override
+	public void runOpMode( ) throws InterruptedException {
+		RGBLights lights = new RGBLights( hardwareMap, "blinkin" );
+		lights.showStatus( RGBLights.StatusLights.NORMAL );
+		waitForStart();
+		lights.showStatus( RGBLights.StatusLights.CELEBRATION );
+		while( opModeIsActive() );
+
+	}
+}
