a85dc541b3af859e306d94dc669b444a2eee98a1
Camden Harris <camdenharris@gmail.com>
2022-10-17 22:30:32 -0700

    added some basic functions to MultiaxialArm, and changed up the tester class to actually test it

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/MultiaxialArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/MultiaxialArm.java
@@ -1,7 +1,10 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class MultiaxialArm {
 	final double PULSES_PER_REVOLUTION = 384.5;
@@ -22,5 +25,21 @@ public class MultiaxialArm {
 		rotationMotor = hm.get( DcMotorEx.class, rotationMotorName );
 		lowerMotor = hm.get( DcMotorEx.class, lowerMotorName );
 		upperMotor = hm.get( DcMotorEx.class, upperMotorName );
+
+		rotationMotor.setZeroPowerBehavior( DcMotorEx.ZeroPowerBehavior.BRAKE );
+		lowerMotor.setZeroPowerBehavior( DcMotorEx.ZeroPowerBehavior.BRAKE );
+		upperMotor.setZeroPowerBehavior( DcMotorEx.ZeroPowerBehavior.BRAKE );
+	}
+
+	public void controlMotors (double rotatePower, double lowerPower, double upperPower) {
+		rotationMotor.setPower( rotatePower );
+		lowerMotor.setPower( lowerPower );
+		upperMotor.setPower( upperPower );
+	}
+
+	public void printTelemetry( Telemetry t ) {
+		t.addData( "Rotate Power:", rotationMotor.getPower());
+		t.addData( "Lower Power:", lowerMotor.getPower());
+		t.addData( "Upper Power:", upperMotor.getPower());
 	}
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/MultiaxialArmTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/MultiaxialArmTest.java
@@ -5,67 +5,26 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.teamcode.subsystems.MultiaxialArm;
+
 @TeleOp(name = "MultiaxialArmTest", group = "Tests" )
 public class MultiaxialArmTest extends OpMode {
 
-	CRServo spinnyServo;
-	Servo lowerServo;
-	Servo upperServo;
-
-	double spinnyServoPower = 0;
-	double lowerServoPosition;
-	double upperServoPosition;
+	MultiaxialArm arm;
 
 	@Override
 	public void init( ) {
-		spinnyServo = hardwareMap.crservo.get( "spinnyServo" );
-		lowerServo = hardwareMap.servo.get( "lowerServo" );
-		upperServo = hardwareMap.servo.get( "upperServo" );
-
-		lowerServoPosition = lowerServo.getPosition();
-		upperServoPosition = upperServo.getPosition();
+		arm = new MultiaxialArm( hardwareMap );
 	}
 
 	@Override
 	public void loop( ) {
-		if(gamepad1.a)
-			spinnyServoPower = 0.25;
-		else if ( gamepad1.b )
-			spinnyServoPower = -0.25;
-		else
-			spinnyServoPower = 0;
 
+		double armRotatePower = gamepad1.left_stick_x;
+		double lowerPower = gamepad1.left_stick_y;
+		double upperPower = gamepad1.right_stick_y;
 
-		if(gamepad1.dpad_up)
-			lowerServoPosition += 0.005;
-		else if ( gamepad1.dpad_down )
-			lowerServoPosition -= 0.005;
-
-		if(lowerServoPosition > 1)
-			lowerServoPosition = 1;
-		else if(lowerServoPosition < 0)
-			lowerServoPosition = 0;
-
-		if(gamepad1.x)
-			upperServoPosition += 0.005;
-		else if ( gamepad1.y )
-			upperServoPosition -= 0.005;
-
-		if(upperServoPosition > 1)
-			upperServoPosition = 1;
-		else if(upperServoPosition < 0)
-			upperServoPosition = 0;
-
-		spinnyServo.setPower(spinnyServoPower);
-		lowerServo.setPosition(lowerServoPosition);
-		upperServo.setPosition(upperServoPosition);
-		printTelemetry();
-	}
-
-	public void printTelemetry() {
-		telemetry.addLine("Spinny Servo Power: " + spinnyServoPower);
-		telemetry.addLine("Lower Servo Position: " + lowerServoPosition);
-		telemetry.addLine("Upper Servo Position: " + upperServoPosition);
-		telemetry.update();
+		arm.controlMotors( armRotatePower, lowerPower, upperPower );
+		arm.printTelemetry( telemetry );
 	}
 }
