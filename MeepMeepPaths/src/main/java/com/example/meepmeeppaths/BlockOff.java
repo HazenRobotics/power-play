57188fd2316a07844a6c467e9805976c68c66d6a
ConnerBrunner <Con-bru000@g.rentonschools.us>
2022-10-25 20:07:21 -0700

    BlockOff Done

---
+++ MeepMeepPaths/src/main/java/com/example/meepmeeppaths/BlockOff.java
@@ -0,0 +1,30 @@
+package com.example.meepmeeppaths;
+
+import com.example.meepmeeppaths.teamcodeRequirements.PPField;
+import com.example.meepmeeppaths.teamcodeRequirements.LifterBot;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.noahbres.meepmeep.roadrunner.DriveShim;
+import com.noahbres.meepmeep.roadrunner.trajectorysequence.TrajectorySequence;
+
+public class BlockOff implements MeepMeepPath {
+
+	float x = -(float) (2 * tileSize - LifterBot.ROBOT_WIDTH / 2 + 3 * tileConnector / 2);
+	float y = PPField.HALF_FIELD - LifterBot.ROBOT_LENGTH / 2;
+	Pose2d conePose = new Pose2d( -tileSize / 2, -(tileSize * 3)+robotLength, Math.toRadians( 180 ) );
+
+
+	@Override
+	public TrajectorySequence getTrajectorySequence( DriveShim drive ) {
+		return drive.trajectorySequenceBuilder( new Pose2d( x, y, Math.toRadians( 90 ) ) )
+				.lineToConstantHeading(new Vector2d(-tileSize*2,y))
+				//drop cone
+				.lineToLinearHeading( conePose )
+				//grab cone
+				.lineToLinearHeading( new Pose2d( -robotLength,0,Math.toRadians( 0 )) )
+				.lineToLinearHeading( conePose )
+				.lineToLinearHeading(new Pose2d( -(tileSize*2)+robotLength,0 , Math.toRadians(0)))
+				.build( );
+	}
+
+}
--- MeepMeepPaths/src/main/java/com/example/meepmeeppaths/circutWCycle.java
+++ MeepMeepPaths/src/main/java/com/example/meepmeeppaths/circutWCycle.java
@@ -7,17 +7,19 @@ import com.example.meepmeeppaths.teamcodeRequirements.PPField;
 import com.noahbres.meepmeep.roadrunner.DriveShim;
 import com.noahbres.meepmeep.roadrunner.trajectorysequence.TrajectorySequence;
 
-public class RedSimpleCycle implements MeepMeepPath {
+public class circutWCycle implements MeepMeepPath {
 
-	float x = 2 * PPField.TILE_SIZE - LifterBot.ROBOT_WIDTH / 2 + 3 * PPField.TILE_CONNECTOR / 2;
+	float x = (float) (2 * tileSize - LifterBot.ROBOT_WIDTH / 2 + 3 * tileConnector / 2);
 	float y = PPField.HALF_FIELD - LifterBot.ROBOT_LENGTH / 2;
+	Pose2d conePose = new Pose2d( -tileSize / 2, -tileSize * 3, Math.toRadians( 180 ) );
+
 
 	@Override
 	public TrajectorySequence getTrajectorySequence( DriveShim drive ) {
-		return drive.trajectorySequenceBuilder( new Pose2d( x, y, Math.toRadians( 270 ) ) )
-
-
-				.lineTo( new Vector2d( 0, -16 ) )
+		return drive.trajectorySequenceBuilder( new Pose2d( x, y, Math.toRadians( 90 ) ) )
+				.lineToConstantHeading(new Vector2d(-tileSize*2,-tileSize*2))
+				//drop cone
+				.lineToLinearHeading( conePose )
 				.build( );
 	}
 
