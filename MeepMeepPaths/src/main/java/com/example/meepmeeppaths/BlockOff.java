977545cdedef8238d8bc271985f9d9090a85c3fe
ConnerBrunner <Con-bru000@g.rentonschools.us>
2022-10-27 14:10:07 -0700

    starting mirror

--- MeepMeepPaths/src/main/java/com/example/meepmeeppaths/BlockOff.java
+++ MeepMeepPaths/src/main/java/com/example/meepmeeppaths/BlockOff.java
@@ -9,22 +9,32 @@ import com.noahbres.meepmeep.roadrunner.trajectorysequence.TrajectorySequence;
 
 public class BlockOff implements MeepMeepPath {
 
-	float x = -(float) (2 * totalTitle - LifterBot.ROBOT_WIDTH / 2 + 3 * tileConnector / 2);
-	float y = -(PPField.HALF_FIELD - LifterBot.ROBOT_LENGTH / 2);
-	Pose2d conePose = new Pose2d( -totalTitle *2.5, -totalTitle/2, Math.toRadians( 180 ) );
+
 
 
 	@Override
 	public TrajectorySequence getTrajectorySequence( DriveShim drive ) {
+		return getFlipedTrajectorySequence( drive,1,1 );
+	}
+
+	@Override
+	public TrajectorySequence getFlipedTrajectorySequence( DriveShim drive, double xFlip, double yFlip ) {
+		double cycledist = totalTitle*1.5;
+		float x = -(float) ((2 * totalTitle - LifterBot.ROBOT_WIDTH / 2 + 3 * tileConnector / 2)*xFlip);
+		float y = (float) (-(PPField.HALF_FIELD - LifterBot.ROBOT_LENGTH / 2)*xFlip);
+		double  xFlipR = -((xFlip-1)/2);
+		double yFlipR = -((xFlip-1)/2);
+		Pose2d conePose = new Pose2d( (-totalTitle *2.5)*xFlip, (-totalTitle/2)*xFlip, Math.toRadians( 180+(180*xFlipR) ) );
 		return drive.trajectorySequenceBuilder( new Pose2d( x, y, Math.toRadians( 90 ) ) )
 				.lineToConstantHeading( new Vector2d( x-(totalTitle/2), y ) )
 				//drop cone
 				.lineToConstantHeading( new Vector2d( x-totalTitle,y ) )
 				.lineToLinearHeading( conePose )
 				//grab cone
-				.lineToLinearHeading( new Pose2d( 0, -totalTitle/2, Math.toRadians( 90 ) ) )
+				.lineToLinearHeading( new Pose2d( 0, -totalTitle/2*yFlip, Math.toRadians( 90+(180*yFlipR) ) ) )
 				.lineToLinearHeading( conePose )
-				.lineToLinearHeading( new Pose2d( -totalTitle*2, -robotLength, Math.toRadians( 90 ) ) )
+				//grab cone
+				.lineToLinearHeading( new Pose2d( -totalTitle*2*xFlip, -robotLength*yFlip, Math.toRadians( 90+(180*yFlipR)  ) ) )
 				.build( );
 	}
 
--- MeepMeepPaths/src/main/java/com/example/meepmeeppaths/CircutDouble.java
+++ MeepMeepPaths/src/main/java/com/example/meepmeeppaths/CircutDouble.java
@@ -8,11 +8,20 @@ import com.noahbres.meepmeep.roadrunner.DriveShim;
 import com.noahbres.meepmeep.roadrunner.trajectorysequence.TrajectorySequence;
 
 public class CircutDouble implements MeepMeepPath {
-	float x = (float) (2 * totalTitle - LifterBot.ROBOT_WIDTH / 2 + 3 * tileConnector / 2);
-	float y = PPField.HALF_FIELD - LifterBot.ROBOT_LENGTH / 2;
-	Pose2d conePose = new Pose2d( -totalTitle *2.5, -totalTitle/2, Math.toRadians( 0 ) );
+
 	@Override
 	public TrajectorySequence getTrajectorySequence( DriveShim drive ) {
+
+	}
+
+	@Override
+	public TrajectorySequence getFlipedTrajectorySequence( DriveShim drive, double xFlip, double yFlip ) {
+		double cycledist = totalTitle*1.5;
+		float x = -(float) ((2 * totalTitle - LifterBot.ROBOT_WIDTH / 2 + 3 * tileConnector / 2)*xFlip);
+		float y = (float) (-(PPField.HALF_FIELD - LifterBot.ROBOT_LENGTH / 2)*xFlip);
+		double  xFlipR = -((xFlip-1)/2);
+		double yFlipR = -((xFlip-1)/2);
+		Pose2d conePose = new Pose2d( (-totalTitle *2.5)*xFlip, (-totalTitle/2)*xFlip, Math.toRadians( 180+(180*xFlipR) ) );
 		return drive.trajectorySequenceBuilder( new Pose2d( x, y, Math.toRadians( 90 ) ) )
 				.lineToConstantHeading( new Vector2d( x-(totalTitle/2), y ) )
 				//drop cone
