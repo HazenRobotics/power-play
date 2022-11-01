1f9e60231cfac39391d9f828e04dc1e3d08eddfc
Connerbruner <72578768+Connerbruner@users.noreply.github.com>
2022-11-01 16:47:34 -0700

    ClawOffSet

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Claw.java
@@ -1,12 +1,16 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+
+import org.apache.commons.math3.geometry.euclidean.threed.Vector3D;
 
 public class Claw {
 
 	Servo leftServo;
 	Servo rightServo;
+	Vector3D clawOffSet = new Vector3D( 15.5,1.075,2 );
 
 	public Claw( HardwareMap hw ) {
 		this( hw, "left", "right" );
