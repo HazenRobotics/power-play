ede03107bb700434cbcd66aabc3c9682525f9593
Sam DePoule <samdepoule@gmail.com>
2022-11-21 16:43:39 -0800

    Updated motor test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/MotorTest.java
@@ -4,26 +4,57 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.teamcode.utils.GamepadEvents;
+
 @TeleOp(name = "MotorTest", group = "TeleOp")
-@Disabled
+//@Disabled
 public class MotorTest extends OpMode {
 
-    DcMotor motor;
+	DcMotor[] motor;
+	String[] motorNames = { "frontLeft", "backLeft", "frontRight", "backRight", "lift", "perp", "para" };
 
-    @Override
-    public void init() {
-        motor = hardwareMap.get( DcMotor.class, "motor" );
-    }
+	GamepadEvents controller;
+	int selected = 0;
+	int maxSelect = motorNames.length - 1;
 
-    @Override
-    public void loop() {
-        if (gamepad1.a)
-            motor.setPower(-0.25);
-        else if (gamepad1.b)
-            motor.setPower(0.25);
+	@Override
+	public void init( ) {
+		controller = new GamepadEvents( gamepad1 );
+		for( int i = 0; i < motorNames.length; i++ )
+			motor[i] = hardwareMap.get( DcMotorEx.class, motorNames[i] );
+	}
 
-        motor.setPower(0);
-    }
+	@Override
+	public void loop( ) {
+
+		if( controller.dpad_up.onPress( ) ) {
+			increment( 1, maxSelect );
+		} else if( controller.dpad_down.onPress( ) ) {
+			increment( -1, maxSelect );
+		}
+
+		telemetry.addLine( "selected: (" + selected + ") " + motorNames[selected] );
+		for( int i = 0; i < motorNames.length; i++ ) {
+			telemetry.addLine( motorNames[i] + " position: " + motor[i].getCurrentPosition( ) );
+		}
+		telemetry.update( );
+		controller.update( );
+	}
+
+	public void increment( int increment, int max ) {
+		increment( increment, 0, max );
+	}
+
+	public void increment( int increment, int min, int max ) {
+		selected += increment;
+		while( selected < min )
+			max -= selected;
+
+		if( selected > max )
+			selected %= max;
+	}
 }
