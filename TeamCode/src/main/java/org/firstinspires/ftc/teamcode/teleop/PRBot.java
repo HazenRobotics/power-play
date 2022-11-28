e5b04d40217fd8d1778f5e27e64335e7ac0cd4f2
ConnerBrunner <Con-bru000@g.rentonschools.us>
2022-11-27 20:35:33 -0800

    Fell down the stairs New PR bot Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/PRBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/PRBot.java
@@ -1,43 +1,36 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "PR bot", group = "TeleOp")
-@Disabled
 
 public class PRBot extends OpMode {
 
-	DcMotor frontLeftMotor, backLeftMotor, frontRightMotor, backRightMotor, launcherLeft, launcherRight;
-	Servo launcherServo;
-	CRServo leftEye, rightEye;
-	boolean aWasPressed = false;
-
+	DcMotor left,right, launcherLeft, launcherRight,bell;
+	Servo blocker;
+	double blockedPose = 0.5;
+	double openPose = 0.75;
 	@Override
 	public void init( ) {
 
 		telemetry.addData( "Mode", "Initiating robot..." );
 		telemetry.update( );
 
-		frontLeftMotor = hardwareMap.get( DcMotor.class, "frontLeft" );
-		backLeftMotor = hardwareMap.get( DcMotor.class, "backLeft" );
-		frontRightMotor = hardwareMap.get( DcMotor.class, "frontRight" );
-		backRightMotor = hardwareMap.get( DcMotor.class, "backRight" );
+		left = hardwareMap.get( DcMotor.class, "left" );
+		right = hardwareMap.get( DcMotor.class, "right" );
+		blocker = hardwareMap.get( Servo.class,"blocker" );
 
 		launcherLeft = hardwareMap.get( DcMotor.class, "launchLeft" );
 		launcherRight = hardwareMap.get( DcMotor.class, "launchRight" );
 
-		leftEye = hardwareMap.get(CRServo.class, "leftEye");
-		rightEye = hardwareMap.get(CRServo.class, "rightEye");
-		launcherServo = hardwareMap.servo.get( "launcher" );
+		bell = hardwareMap.get( DcMotor.class, "bell" );
 
-		launcherServo.setPosition( 0.5 );
-		backRightMotor.setDirection( DcMotorSimple.Direction.REVERSE );
+
+		right.setDirection( DcMotorSimple.Direction.REVERSE );
 		launcherLeft.setDirection( DcMotorSimple.Direction.REVERSE );
 
 		telemetry.addData( "Mode", "waiting for start" );
@@ -47,35 +40,18 @@ public class PRBot extends OpMode {
 	@Override
 	public void loop( ) {
 
-		move( -gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x );
+		setMotorPower( gamepad1.left_stick_y,gamepad1.right_stick_y );
 
-		if( aWasPressed && !gamepad1.a )
-			ring( );
-
-//		for normal controller
-//		double power = gamepad1.left_trigger + gamepad1.right_trigger;
-
-//		for conner's odd controller
-		double power = gamepad1.right_bumper ? 1 : 0;
-
-
-		if( power > 1 )
-			power = 1;
-
-		if( gamepad1.start ) {
-			launcherLeft.setPower( 1 );
-			launcherRight.setPower( 1 );
-			spinEyes( 1 );
-			ring( );
-			waitRobot( 200 );
-		}
-
+		double power = gamepad1.left_trigger + gamepad1.right_trigger;
 		launcherLeft.setPower( power );
 		launcherRight.setPower( power );
 
-		spinEyes( gamepad1.left_bumper ? 1 : (gamepad1.left_bumper ? -1 : power ) );
-
-		aWasPressed = gamepad1.a;
+		if( gamepad1.a && blocker.getPosition() != blockedPose) {
+			blocker.setPosition( blockedPose );
+		}
+		if(!gamepad1.a && blocker.getPosition() != openPose) {
+			blocker.setPosition( openPose );
+		}
 
 		telemetry.update( );
 	}
@@ -84,38 +60,20 @@ public class PRBot extends OpMode {
 	 * set directional power
 	 *
 	 * @param drive  power
-	 * @param strafe strafe power
-	 * @param rotate power
 	 */
-	public void move( double drive, double strafe, double rotate ) {
-		double frontLeftPower = drive + strafe + rotate;
-		double backLeftPower = drive - strafe + rotate;
-		double frontRightPower = drive - strafe - rotate;
-		double backRightPower = drive + strafe - rotate;
-
-		setMotorPower( frontLeftPower, backLeftPower, frontRightPower, backRightPower );
-	}
 
 	/**
 	 * set individual power
 	 *
-	 * @param frontLeftPower  power
-	 * @param backLeftPower   power
-	 * @param frontRightPower power
-	 * @param backRightPower  power
+	 * @param leftPower  power
+	 * @param rightPower   power
 	 */
-	public void setMotorPower( double frontLeftPower, double backLeftPower, double frontRightPower, double backRightPower ) {
-		frontLeftMotor.setPower( frontLeftPower );
-		backLeftMotor.setPower( backLeftPower );
-		frontRightMotor.setPower( frontRightPower );
-		backRightMotor.setPower( backRightPower );
+	public void setMotorPower( double leftPower , double rightPower ) {
+		left.setPower( leftPower );
+		right.setPower( rightPower );
 	}
 
-	public void ring( ) {
-		launcherServo.setPosition( 0.75 );
-		waitRobot( 400 );
-		launcherServo.setPosition( 0.5 );
-	}
+
 
 	public void waitRobot( int mills ) {
 		long startTime = System.currentTimeMillis( );
@@ -123,8 +81,8 @@ public class PRBot extends OpMode {
 			telemetry.update( );
 	}
 
-	public void spinEyes(double power) {
-		leftEye.setPower( power );
-		rightEye.setPower( power );
-	}
+//	public void spinEyes(double power) {
+//		leftEye.setPower( power );
+//		rightEye.setPower( power );
+//	}
 }
