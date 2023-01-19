b855fd55c6a7024271fbe8db5e0d92935b23b5fc
ConnerBrunner <Con-bru000@g.rentonschools.us>
2023-01-18 20:10:59 -0800

    better HZ

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/HertzTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/HertzTest.java
@@ -3,8 +3,11 @@ package org.firstinspires.ftc.teamcode.tests;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
+import org.firstinspires.ftc.teamcode.teleop.MiniTeleOp;
+
 public class HertzTest extends LinearOpMode {
-	OpMode target;
+
+	MiniTeleOp target = new MiniTeleOp( );
 	long cycles = 0;
 	long timeRan = 0;
 	long hertz = 0;
@@ -13,13 +16,14 @@ public class HertzTest extends LinearOpMode {
 	public void runOpMode( ) throws InterruptedException {
 		target.init( );
 		long startTime = System.currentTimeMillis( );
-		while( this.opModeIsActive() ) {
+		while( this.opModeIsActive( ) ) {
 			target.loop( );
 			cycles++;
 			timeRan = System.currentTimeMillis( ) - startTime;
-			hertz = cycles/(timeRan/1000);
-			target.telemetry.addData( "HZ:",hertz );
-			target.telemetry.addData( "Time eplased",timeRan );
+			hertz = cycles / (timeRan / 1000);
+			target.telemetry.addData( "HZ:", hertz );
+			target.telemetry.addData( "Time eplased", timeRan );
 		}
 	}
 }
+
