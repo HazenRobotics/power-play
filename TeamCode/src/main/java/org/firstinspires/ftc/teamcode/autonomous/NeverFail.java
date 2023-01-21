3052769a19c5a1ec060c4d7b7186f76e7c83846e
sirpoetvic <86131430+sirpoetvic@users.noreply.github.com>
2023-01-21 13:01:48 -0800

    park auto @comp prototype

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/NeverFail.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/NeverFail.java
@@ -14,66 +14,59 @@ import org.firstinspires.ftc.teamcode.vision.pipelines.AprilTagDetectionPipeline
 //@Disabled
 public class NeverFail extends LinearOpMode {
 
-	MiniBot robot;
-	MecanumDrive drive;
+    MiniBot robot;
+    MecanumDrive drive;
 
-	@Override
-	public void runOpMode( ) {
+    @Override
+    public void runOpMode() {
 
-		robot = new MiniBot( this );
-		drive = new MecanumDrive( hardwareMap );
-		AprilTagsUtil detector = new AprilTagsUtil( hardwareMap, "webcam1", telemetry );
-		detector.init( );
+        robot = new MiniBot(this);
+        drive = new MecanumDrive(hardwareMap);
+        AprilTagsUtil detector = new AprilTagsUtil(hardwareMap, "webcam1", telemetry);
+        detector.init();
 
-		DcMotorEx[] motors = {
-				drive.frontLeft,
-				drive.backLeft,
-				drive.frontRight,
-				drive.backRight,
-		};
-		for( DcMotorEx d : motors ) {
-			d.setMode( DcMotor.RunMode.RUN_TO_POSITION );
-		}
-		for( DcMotorEx d : motors ) {
-			d.setTargetPosition( drive.convertDistTicks( 24 ) );
-		}
-		AprilTagDetectionPipeline.SignalPosition park;
-		while( !isStopRequested( ) && !isStarted( ) ) {
-			telemetry.addData( "Element position", robot.signalUtil.getSignalPosition( ) );
-			telemetry.update( );
-		}
-		waitForStart( );
-		park = robot.signalUtil.getSignalPosition( );
-		for( DcMotorEx d : motors ) {
-			d.setPower( 0.25 );
-		}
-		while( motors[0].isBusy( ) ) ;
+        DcMotorEx[] motors = {
+                drive.frontLeft,
+                drive.backLeft,
+                drive.frontRight,
+                drive.backRight,
+        };
 
-		if( park != AprilTagDetectionPipeline.SignalPosition.MIDDLE ) {
-			for( DcMotorEx d : motors ) {
-				d.setMode( DcMotor.RunMode.RESET_ENCODERS );
-			}
-			for( DcMotorEx d : motors ) {
-				d.setMode( DcMotor.RunMode.RUN_TO_POSITION );
-			}
-			for( DcMotorEx d : motors ) {
-				d.setTargetPosition( drive.convertDistTicks( 24 ) );
-			}
-			if( park == AprilTagDetectionPipeline.SignalPosition.LEFT ) {
-				motors[0].setPower( -0.25 );
-				motors[1].setPower( 0.25 );
-				motors[2].setPower( -0.25 );
-				motors[3].setPower( 0.25 );
-			}
-			if( park == AprilTagDetectionPipeline.SignalPosition.RIGHT ) {
-				motors[0].setPower( 0.25 );
-				motors[1].setPower( -0.25 );
-				motors[2].setPower( 0.25 );
-				motors[3].setPower( -0.25 );
-			}
-			while( motors[0].isBusy( ) ) ;
-		}
-	}
+        AprilTagDetectionPipeline.SignalPosition park;
+        while (!isStopRequested() && !isStarted()) {
+            telemetry.addData("Element position", robot.signalUtil.getSignalPosition());
+            telemetry.update();
+        }
+        waitForStart();
+        park = robot.signalUtil.getSignalPosition();
+        for (DcMotorEx d : motors) {
+            d.setPower(0.25);
+        }
+        waitRobot(2000);
+
+        while (motors[0].isBusy()) ;
+
+        if (park != AprilTagDetectionPipeline.SignalPosition.MIDDLE) {
+            if (park == AprilTagDetectionPipeline.SignalPosition.LEFT) {
+                motors[0].setPower(-0.25);
+                motors[1].setPower(0.25);
+                motors[2].setPower(-0.25);
+                motors[3].setPower(0.25);
+            }
+            if (park == AprilTagDetectionPipeline.SignalPosition.RIGHT) {
+                motors[0].setPower(0.25);
+                motors[1].setPower(-0.25);
+                motors[2].setPower(0.25);
+                motors[3].setPower(-0.25);
+            }
+            waitRobot(2000);
+        }
+    }
+
+    public static void waitRobot(int mills) {
+        long startTime = System.currentTimeMillis();
+        while ((startTime + mills) > System.currentTimeMillis()) ;
+    }
 
 }
 
