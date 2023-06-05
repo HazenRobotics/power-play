476b8200faf3b1232dc75e773d763776ae439692
Aidan Tran <aidandtran3@yahoo.com>
2023-06-05 15:26:21 -0700

    AidenTTeleop completed. ConnersArm (servo subsystem) Completed. ConnersEyes (Continous rotation servo subsystem) Completed.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AidenYAuto.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class AidenYAuto extends LinearOpMode {
+
+	@Override
+	public void runOpMode( ) throws InterruptedException {
+
+	}
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/AidanTTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/AidanTTeleop.java
@@ -7,6 +7,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.firstinspires.ftc.teamcode.subsystems.ConnersArm;
+import org.firstinspires.ftc.teamcode.subsystems.ConnersEyes;
 import org.firstinspires.ftc.teamcode.utils.GamepadEvents;
 @TeleOp(name = "AidanTTeleop", group = "TeleOp")
 public class AidanTTeleop extends OpMode {
@@ -15,7 +16,7 @@ public class AidanTTeleop extends OpMode {
 	String[] motorNames = {"fl","bl","fr","br"};
 	//SubSystem for arm
 	ConnersArm arm;
-
+	ConnersEyes eyes;
 	@Override
 	public void init( ) {
 		for(int i=0;i<motors.length;i++){
@@ -25,19 +26,21 @@ public class AidanTTeleop extends OpMode {
 		motors[3].setDirection( DcMotorSimple.Direction.REVERSE );
 		controller = new GamepadEvents( gamepad1 );
 		arm = new ConnersArm(hardwareMap);
+		eyes = new ConnersEyes(hardwareMap);
 	}
 
 	@Override
 	public void loop( ) {
-		double drive = -controller.left_stick_y;
-		double strafe = controller.left_stick_x;
-		double rotate = controller.right_stick_x;
+		double drive = controller.left_stick_y;
+		double strafe = -controller.left_stick_x;
+		double rotate = -controller.right_stick_x;
 
 		motors[0].setPower(drive + strafe + rotate);
 		motors[1].setPower(drive - strafe + rotate);
 		motors[2].setPower(drive - strafe - rotate);
 		motors[3].setPower(drive + strafe - rotate);
 
+		eyes.rotate(controller.left_trigger.getTriggerValue(),controller.right_trigger.getTriggerValue());
 		if (controller.a.onPress()){
 			arm.move();
 		}
